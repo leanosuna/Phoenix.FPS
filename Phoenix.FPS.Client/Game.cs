1076e44dd2b118a218a69456a78c234b83dcc6f4
Leandro Osuna <leandrog.osuna@gmail.com>
2026-06-14 01:20:42 -0300

    first

---
+++ Phoenix.FPS.Client/Game.cs
@@ -0,0 +1,143 @@
+﻿using Phoenix.Framework;
+using Phoenix.Framework.AssetImport;
+using Phoenix.Framework.Cameras;
+using Phoenix.Framework.Rendering;
+using Phoenix.Framework.Rendering.Geometry.Model;
+using Phoenix.Framework.Rendering.Geometry.Model.Meshes;
+using Phoenix.Framework.Rendering.Geometry.Vertices;
+using Phoenix.Framework.Rendering.GUI;
+using Phoenix.Framework.Rendering.Textures;
+using Phoenix.Framework.ShaderHelpers;
+using Phoenix.Framework.Maths;
+
+using Silk.NET.Input;
+using Silk.NET.Maths;
+using Silk.NET.OpenGL;
+using System.Numerics;
+using Phoenix.Framework.Rendering.Primitives;
+//using Cube as Phoenix
+namespace Phoenix.FPS.Client
+{
+    public class Game : PhoenixGame
+    {
+        Matrix4x4 _cubeWorld = Matrix4x4.Identity;
+
+        protected override void InitialLoadScreen()
+        {
+            UI.DrawText("Custom startup screen", 
+                position: Vector2.Zero, 
+                color: Vector4.One, 
+                size: 30);
+        }
+        ShaderModel shaderModel;
+        GLTextureCube cubeTex;
+        ShaderSkybox ShaderSkybox;
+        Cube cubePrim;
+        protected override void Initialize()
+        {
+            //Primitive.SetGL(GL);
+            Window.VSync = false;
+            Window.UpdatesPerSecond = 0;
+            Log.Enabled = true;
+            Log.ConsoleWrite = true;
+            
+            shaderModel = new ShaderModel();
+            shaderModel.AttachUBO(CommonUboHandle, "CommonData");
+
+            ShaderSkybox = new ShaderSkybox();
+            ShaderSkybox.AttachUBO(CommonUboHandle, "CommonData");
+
+            //model = AssetLoader.LoadModel("3D/player/swatguy/Ch15_nonPBR");
+
+            var basePath = "Textures/skybox/";
+            cubeTex = AssetLoader.LoadTextureCube(
+                [basePath + "right",
+                basePath + "left",
+                basePath + "top",
+                basePath + "bottom",
+                basePath + "front",
+                basePath + "back"]);
+
+
+            // Asset loading ...
+            var cam =  new FreeCamera(
+                game: this,
+                position: new Vector3(0, 0, -10),
+                yaw: MathHelper.PiOver2,
+                pitch: 0f,
+                fov: MathHelper.PiOver2,
+                nearPlane: 0.1f, 
+                farPlane: 1000f,
+                aspectRatio: WindowWidth / (float)WindowHeight
+            );
+            cam.SetMoveKeys(Key.W, Key.S, Key.A, Key.D, Key.Space, Key.AltLeft, Key.ShiftLeft, 2f);
+            cam.SetPitchYawKeys(Key.Up, Key.Down, Key.Left, Key.Right, Vector2.One);
+            cam.MouseAim = true;
+
+            Camera = cam;
+
+            Gizmos.Enabled = true;
+
+            cubePrim = Cube.Create(new InfoCube { MeshPrimitiveType = PrimitiveType.Triangles, Uv = true });
+        }
+                
+        protected override void Update(double deltaTime)
+        {
+            
+            if (InputManager.KeyDown(Key.Escape))
+                Stop();
+
+            // Game logic ...
+            var t = (float)Graphics.Time;
+            ((FreeCamera)Camera).Update(deltaTime);
+            _cubeWorld = Matrix4x4.CreateScale(5f)
+                * MathHelper.RotationMxFromYawPitchRoll(t, MathF.Sin(t), MathF.Cos(t));
+        }
+
+        protected override void Render(double deltaTime)
+        {
+            // Render logic ...
+            Graphics.SetClearColor(new Vector4(0.1f, 0.1f, 0.1f, 1));
+            Graphics.ClearRenderTarget();
+
+            //Gizmos.AddCube(_cubeWorld, Vector3.One);
+            //Gizmos.AddCube()
+            Graphics.SetFaceCulling(false);
+            Graphics.SetDepthTest(true, GLEnum.Lequal);
+           
+
+            shaderModel.World.Set(_cubeWorld);
+            shaderModel.Tex.Set(cubeTex);
+            
+            ShaderSkybox.Use();
+            ShaderSkybox.Tex.Set(cubeTex);
+
+            Graphics.SetFaceCulling(true, GLEnum.Front);
+            cubePrim.Draw();
+
+            Gizmos.AddCube(_cubeWorld, Vector3.One);
+            Gizmos.AddAxisLines(400);
+        }
+
+        protected override void RenderUI()
+        {
+            // UI pass...
+            UI.DrawRAlignedText($"FPS {(int)Graphics.FPS_SAMPLE}",
+                position: new Vector2(WindowWidth, 10),
+                color: Vector4.One,
+                size: 20);
+
+            
+        }
+
+        protected override void OnWindowResize(Vector2 size)
+        {
+            // Something needs resizing...
+        }
+
+        protected override void OnClose()
+        {
+            // Something needs disposing...
+        }
+    }
+}
---
+++ Phoenix.FPS.Client/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Phoenix.FPS.Client;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var game = new Game();
+        game.Run();
+    }
+}
